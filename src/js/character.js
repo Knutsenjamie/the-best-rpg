173cc90c7fb4992dc7a49e9b519a2bf5c13e7f30
Cristina Plesa <cristinaplesa8@gmail.com>
2021-04-15 16:02:41 -0700

    add other character types to characterType function.
    Co-authored-by: Jamie Knutsen <knutsenjamie@yahoo.com>
    Co-authored-by: Thomas Russell <t.p.russell19@gmail.com>

--- src/js/character.js
+++ src/js/character.js
@@ -17,11 +17,52 @@ export default class Character {
   characterType() {
     if (this.type === "Mage") {
       this.health = 100
+      this.meleeAttack = 50
       this.magicAttack = 20
       this.bowAttack = 0
       this.magicka = 200
       this.endurance = 100
       this.armor = 150
+    } else if (this.type === "Rogue") {
+      this.health = 100
+      this.meleeAttack = 10
+      this.magicAttack = 0
+      this.bowAttack = 50
+      this.magicka = 0
+      this.endurance = 150
+      this.armor = 200
+    } else if (this.type === "Warrior") {
+      this.health = 100
+      this.meleeAttack = 150
+      this.magicAttack = 0
+      this.bowAttack = 10
+      this.magicka = 0
+      this.endurance = 200
+      this.armor = 250
+    } else if (this.type === "Zombie") {
+      this.health = 200
+      this.meleeAttack = 100
+      this.magicAttack = 0
+      this.bowAttack = 0
+      this.magicka = 25
+      this.endurance = 100
+      this.armor = 250
+    } else if (this.type === "Skeleton") {
+      this.health = 50
+      this.meleeAttack = 25
+      this.magicAttack = 20
+      this.bowAttack = 0
+      this.magicka = 0
+      this.endurance = 10
+      this.armor = 15
+    } else if (this.type === "Orc") {
+      this.health = 300
+      this.meleeAttack = 250
+      this.magicAttack = 200
+      this.bowAttack = 0
+      this.magicka = 200
+      this.endurance = 250
+      this.armor = 300
     }
   }
 
